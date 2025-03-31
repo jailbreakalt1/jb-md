4c012402d69eb5fa1490bf0c598814f187d4deae
jailbreakalt1 <ndinikani@gmail.com>
2025-03-31 20:39:38 +0200

    Update config.js

--- config.js
+++ config.js
@@ -8,4 +8,5 @@ function convertToBool(text, fault = "true") {
 module.exports = {
   SESSION_ID: process.env.SESSION_ID || "r1IHmTRb#dFH-1WiNmm3I8yco7pvdmLTItdnIdnZME50OENLMsSY",
   OWNER_NUM: process.env.OWNER_NUM || "263717456159",
+  PREFIX: process.envPREFIX || "/",
 };
